a6c679ddc0f48203ccd0337883c161a797c83580
Marcin Czapulak <zenobiusz.czapulak@gmail.com>
2022-10-28 20:38:30 +0200

    MIA4

---
+++ MIA/L4/Z1L4.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,q;
+int warriors[200000],pom[200001];
+long arrows[200000];
+cin>>n>>q;
+for(int i=0;i<n;i++)
+{
+    cin>>warriors[i];
+}
+pom[0]=0;
+for(int i=1;i<n+1;i++)
+{
+    pom[i]=pom[i-1]+warriors[i-1];
+}
+/*for(int i=0;i<=n;i++)
+{
+    cout<<pom[i]<<" ";
+}*/
+for(int i=0;i<q;i++)
+{
+    cin>>arrows[i];
+}
+int overdmg=0;
+for(int i=0;i<q;i++)
+{
+//bin search upper_bound()
+if(overdmg+arrows[i]>=pom[n])
+{
+    overdmg=0;
+    cout<<n<<"\n";
+}
+else
+{//bin search po pom
+    int l=0;
+    int r=n;
+    while(l<=r)
+    {
+        int mid=(l+r)/2;
+        if(pom[mid]>arrows[i]+overdmg)
+        {
+            r=mid-1;
+        }
+        else
+        {
+            l=mid+1;
+        }
+        if(pom[mid-1]<arrows[i]+overdmg && pom[mid]>arrows[i]+overdmg)
+        {
+            cout<<n-l<<"\n";
+            overdmg=arrows[i]+overdmg-pom[l];
+            break;
+        }
+        
+    }
+    cout<<n-l<<endl;
+}
+}
+return 0;
+}
+/*for(int i=0;i<q;i++)
+{
+    while(arrows[i]>0)
+    {
+        if(idx>=n)
+        {
+            idx=0;
+            for(int j=0;j<n;j++)
+            {
+                warriors[j]=pom[j];
+            }
+        }
+        if(warriors[idx]>0)
+        {
+            warriors[idx]--;
+            arrows[i]--;
+        }
+        else
+        {
+            idx++;
+        }
+    }
+    cout<<idx+1<<endl;
+}*/
