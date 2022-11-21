c4ec8827ec34f8382a9baef06b84da31c09228ae
Marcin Czapulak <zenobiusz.czapulak@gmail.com>
2022-11-21 16:41:43 +0100

    wychodzimy_4zadPrawie

---
+++ ANL/Lista4/Zad4.py
@@ -0,0 +1,24 @@
+from math import sqrt
+from math import sin
+def F(x):
+    return (x**4)-6*(sin(3*x-1))
+
+def f_1(x_0, x_1):
+    wynik = (x_0+x_1)/2
+    while(F(wynik)!=0):
+        if(F(wynik)>0):
+            x_1=wynik
+        else:
+            x_0=wynik
+        wynik=(x_0+x_1)/2
+    return wynik
+#for i in range(iteracje):
+        #wynik = wynik - (((wynik-1)**2)*((wynik-2)**2)) / \
+            #((2*(wynik**3)) - (9*(wynik**2)) + (13*wynik) - 6)
+
+print("Kolejne iteracje f_1 dla x_0 e [-2,-1],x_1 e [-1,0],x_2 e [0,1],x_3 e [1,2]")
+#for i in range(1, 10):
+print("x_0 - ", f_1(-2, -1))
+print("x_1 - ", f_1(-1, 0))
+print("x_2 - ", f_1(0, 1))
+print("x_3 - ", f_1(1, 2))
