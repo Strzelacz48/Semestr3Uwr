506300f7fb1a7f11fb8dcde78dc2710c10dcf7ea
Marcin Czapulak <zenobiusz.czapulak@gmail.com>
2022-11-22 00:02:12 +0100

    Zad 3 zakodzone

--- ANL/Lista4/Zad3.py
+++ ANL/Lista4/Zad3.py
@@ -1,11 +1,10 @@
-def bisection(F, a, b, eps):
+def bisection(F, a, b, it):
     if F(a) * F(b) >= 0:
         print("Bisection method fails.")
         return None
     a_n = a
     b_n = b
-    n = 0
-    while (b_n - a_n) / 2.0 > eps:
+    for i in range(1,it):
         m_n = (a_n + b_n) / 2.0
         if F(m_n) == 0:
             return m_n
@@ -15,9 +14,11 @@ def bisection(F, a, b, eps):
         else:
             a_n = m_n
             b_n = b_n
-        n += 1
+        print("Iteration", i, " m_n :", m_n, "Wynik funkcji : ", F(m_n))
     return (a_n + b_n) / 2.0
+
 def f(x):
     return x-0.49
- 
-print()
+iterations = 27
+wynik=bisection(f, 0, 1, iterations)
+print("Wynik bisekcji po ",iterations," krokach m_n : ",wynik, "f(m_n) : ", f(wynik))
